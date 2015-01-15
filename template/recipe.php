ef10df0c0f020e58d42ae9d89072631e7c075b90
Tim <timgauthier@users.noreply.github.com>
2015-01-15 13:18:31 +0100

    added if for instructions
    The if statement checks if the step starts with a title (anything with "<h" in the tag). Then simply renders as a paragraph instead of a list item.

--- template/recipe.php
+++ template/recipe.php
@@ -1,17 +1,41 @@
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
-
-    <div class="text grid-base">
+  <main >
       <h1><?php echo $page->title()->html() ?></h1>
-      
-      <? if(!$page->ingredients()->empty() ) :?>
-	      <div class="half-grid">
+
+      <section class="row">
+      <? if(!$page->directions()->empty() ) :?>
+      	<div class="half-width">
+	      <h2>Directions</h2>
+	      <ol class="directions">
+		      <?php foreach($page->directions()->yaml() as $instruction): ?>
+		      	<? 
+		      	// This checks if the step starts with a title, if it does it doesn't put a bullet next to it. Great for inline directions!
+		      	if (0 === strpos(kirbytext($instruction['step']), '<h')) :
+		      		echo kirbytext($instruction['step']);
+						else :
+						?>
+					  <li class="step">
+					    <?php echo kirbytext_no_p($instruction['step']) ?>
+					  </li>
+					<?php endif; endforeach ?>
+				</ol>
+				</div>
+			<? else :?>
+			<p>Please add some cooking directions</p>
+			<? endif ?>
+
+			<? if(!$page->ingredients()->empty() ) :?>
+	      <div class="half-width">
 	      <h2>Ingredients</h2>
 	      <ul class="ingredients">
 		      <?php foreach($page->ingredients()->yaml() as $item): ?>
 					  <li class="item">
-					    <?php echo $item['ingredient'] ?> – <?php echo $item['amount'] ?>
+					  <?
+					  	$ingredient = markdown_no_p($item['ingredient']);
+					  	$amount = markdown_no_p($item['amount']);
+				  	?>
+					    <?php echo $ingredient ?> – <?php echo $amount ?>
 					  </li>
 					<?php endforeach ?>
 				</ul>
@@ -19,25 +43,7 @@
 			<? else :?>
 				<p>Please add some ingredients!</p>
 			<? endif ?>
-
-      <? if(!$page->directions()->empty() ) :?>
-      	<div class="half-grid">
-	      <h2>Directions</h2>
-	      <ol class="directions">
-		      <?php foreach($page->directions()->yaml() as $instruction): ?>
-					  <li class="step">
-					    <?php echo $instruction['step'] ?>
-					  </li>
-					<?php endforeach ?>
-				</ol>
-				</div>
-			<? else :?>
-			<p>Please add some cooking directions</p>
-			<? endif ?>
-
-
-    </div>
-
+			</section>
   </main>
 
 <?php snippet('footer') ?>
